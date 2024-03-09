a7372fb361ea764fe8869b03ffb65571ce84a15e
Soumya12346 <155097146+Soumya12346@users.noreply.github.com>
2024-03-09 16:46:22 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -12,14 +12,6 @@ variable "ami" {
   description = "AMI ID for the EC2 instance"
   default     = "ami-0ba259e664698cbfc"
 }
-variable "vpc_id" {
-  description = "vpc-0502f2708ff475f0b"
-}
-
-variable "security_group_ids" {
-  description = "List of security group IDs"
-  type        = list(string)
-}
 
 variable "instance_name" {
   description = "Name tag for the EC2 instance"
