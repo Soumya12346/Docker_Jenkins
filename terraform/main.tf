7a8f6f36a749e1604e863220ab2248938bd4938e
Soumya12346 <155097146+Soumya12346@users.noreply.github.com>
2024-03-09 18:15:38 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  default = "ap-south-1"
+}
+
+resource "aws_instance" "my_instance" {
+  ami           = "ami-0ba259e664698cbfc"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "doccker_terraform_instance"
+  }
+  
+  data "template_file" "web_userdata" {
+   template = file("dockerinstall.sh")
+}
+}
