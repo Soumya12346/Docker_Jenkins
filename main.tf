38b9a74eb5da143e85d3823c403545ec902cfc1e
Soumya12346 <155097146+Soumya12346@users.noreply.github.com>
2024-03-09 17:04:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,15 +2,15 @@ provider "aws" {
   region = var.aws_region
 }
 
- # Create EC2 instance
+# Create EC2 instance
 resource "aws_instance" "my_instance" {
   ami           = var.ami
   instance_type = var.instance_type
   tags = {
     Name = var.instance_name
   }
- provisioner "local-exec" {
-    command = "chmod 777 dockerinstall.sh"
-    command = "./dockerinstall.sh"
+
+  provisioner "local-exec" {
+    command = "chmod +x dockerinstall.sh && ./dockerinstall.sh"
   }
 }
